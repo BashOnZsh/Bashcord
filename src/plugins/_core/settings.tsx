d98819afe3f33e7a057c7b6c29719cce7e5d1770
Ba$h <116464968+BashOnZsh@users.noreply.github.com>
2026-03-13 22:50:47 +0100

    First commit

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -192,8 +192,8 @@ export default definePlugin({
         const equicordEntries: SettingsLayoutNode[] = [
             buildEntry({
                 key: "equicord_main",
-                title: "Equicord",
-                panelTitle: "Equicord Settings",
+                title: "Bashcord",
+                panelTitle: "Bashcord Settings",
                 Component: VencordTab,
                 Icon: MainSettingsIcon
             }),
